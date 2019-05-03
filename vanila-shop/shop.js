06bd7a8419921b21d8a9e91f671befe8ddd7ae0a
Alexander Lysenko <alexlysenko@geeksforless.net>
2019-05-03 21:44:04 +0300

    Add some code

--- vanila-shop/shop.js
+++ vanila-shop/shop.js
@@ -1,3 +1,26 @@
 function Shop(productList) {
     this.productList = productList || {};
+    this.cartWrapper = ( document.querySelector(".js-cart-parent") || {} ).innerHTML;
+    this.oneProductInList = ( document.getElementById("oneProductInList") || {} ).innerHTML;
+    this.listOfProducts = ( document.querySelector("#listOfProducts") || {} ).innerHTML;
+    this.buildProductLIst();
 }
+
+Shop.prototype.buildProductLIst = function(){
+    var listProductsDom = document.createElement("div");
+    var self = this;
+    listProductsDom.innerHTML = this.productList.map(function(val) {
+        var oneElem = document.createElement('div');
+        oneElem.innerHTML = self.oneProductInList;
+        oneElem.querySelector(".js-product-name").textContent = val.name;
+        oneElem.querySelector(".js-product-desc").textContent = val.desc;
+        oneElem.querySelector(".js-product-price").textContent = '$' + val.price;
+        oneElem.querySelector(".js-add-product-button").dataset.prId = val.id;
+        return oneElem.innerHTML;
+    }).join('');
+    document.querySelector(".js-product-list-wrap").innerHTML = listProductsDom.innerHTML;
+};
+
+Shop.prototype.saveShoppingCart = function(){
+    
+};

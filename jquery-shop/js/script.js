0241901ef04a5dacce74b8f8bc7f0e5d3320d94b
Alexander Lysenko <alexlysenko@geeksforless.net>
2019-07-15 23:36:23 +0300

    add

--- jquery-shop/js/script.js
+++ jquery-shop/js/script.js
@@ -1,18 +1,36 @@
 function CartPage(productsList) {
   var _this = this,
-      products = productsList;
+      products = productsList,
+      shoppingList = ;
 
   _this.buildList();
 }
 
 CartPage.prototype.buildList = function() {
-    console.log(products);
-
-
+    var _this = this,
+        parentWrap = jQuery(".js-product-wrap") || "";
+    parentWrap.empty();
     $.each(products,function(index,value){
-      var template = jQuery(".templates .js-one-product").clone();
-      jQuery(template).find(".js-product-name").text(value.name);
-      jQuery(".js-product-wrap").append(template);
-      console.log(template);
+      var template = jQuery(jQuery(".templates .js-one-product").clone());
+      template.find(".js-product-name").text(value.name);
+      template.find(".js-product-desc").text(value.desc);
+      template.find(".js-product-price").text('$' + value.price);
+      template.on("click", function(){
+        _this.addProduct(jQuery(this).index());
+      })
+      parentWrap.append(template);
     });
 }
+
+CartPage.prototype.buildShoppingCart = function() {
+  console.log('buildShoppingCart');
+}
+
+CartPage.prototype.addProduct = function(productId){
+  console.log(products[productId]);
+
+};
+
+CartPage.prototype.saveShopingList = function(){
+  // Local storage - https://tproger.ru/articles/localstorage/
+};

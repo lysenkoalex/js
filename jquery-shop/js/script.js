211d823a24d6f979edb180e2edd97ce1c92f4bdf
Alexander Lysenko <alexlysenko@geeksforless.net>
2019-07-17 23:42:13 +0300

    add

--- jquery-shop/js/script.js
+++ jquery-shop/js/script.js
@@ -1,10 +1,10 @@
 function CartPage(productsList) {
-  var _this = this,
-      products = productsList,
-      shoppingList = _this.getShoppingList();
+  this.products = productsList,
+  this.shoppingList = this.getShoppingList();
 
-  _this.buildList();
-  _this.buildShoppingCart();
+  this.buildList();
+
+  this.buildShoppingCart();
 }
 
 CartPage.prototype.buildList = function() {
@@ -16,22 +16,41 @@ CartPage.prototype.buildList = function() {
       template.find(".js-product-name").text(value.name);
       template.find(".js-product-desc").text(value.desc);
       template.find(".js-product-price").text('$' + value.price);
-      template.on("click", function(){
-        _this.addProduct(jQuery(this).index());
+      template.find(".js-add-product-button").on("click", function(){
+        _this.addProduct(jQuery(this).parents(".js-one-product").index());
       })
       parentWrap.append(template);
     });
 }
 
 CartPage.prototype.buildShoppingCart = function() {
-  var parentWrap = jQuery(".js-shopping-wrap"),
-      templateElement = jQuery(jQuery(".templates .js-one-shopping-line").clone());
+  var _this = this,
+      parentWrap = jQuery(".js-shopping-wrap");
 
-  console.log(templateElement.html());
+  $.each(this.shoppingList, function(index,value){
+      parentWrap.append(_this.renderShoppingLine(value));
+  });
+}
+
+CartPage.prototype.renderShoppingLine = function(indexElement) {
+    var _this = this;
+        templateElement = jQuery(jQuery(".templates .js-one-shopping-line").clone());
+    templateElement.find(".js-product-name").text(products[indexElement.productId].name);
+    templateElement.find(".js-product-price").text("$" + (products[indexElement.productId].price * indexElement.quantity ).toFixed(2));
+    templateElement.find(".js-product-count").text(indexElement.quantity);
+
+    templateElement.find(".js-quantity-plus").on("click", function() {
+      _this.addProduct(indexElement.productId);
+    });
+
+    templateElement.find(".js-quantity-minus").on("click", function() {
+      _this.removeProduct(indexElement.productId);
+    });
+    return templateElement;
 }
 
 CartPage.prototype.addProduct = function(productId){
-  var currentList = this.getShoppingList(),
+  var currentList = this.getShoppingList() || [],
       newLineFlag = true;
 
   for (var i=0; i < currentList.length; i++ ) {
@@ -70,5 +89,5 @@ CartPage.prototype.saveShopingList = function(obj){
 };
 
 CartPage.prototype.getShoppingList = function(){
-    return JSON.parse(localStorage.getItem('shoppingCart')) || [];
+    return JSON.parse(localStorage.getItem('shoppingCart'));
 }

c6739f2a63a9d49e8cf0bbe01d6d0162fb2ad0f8
Alexander Lysenko <alexsmith.ua@gmail.com>
2019-08-07 11:27:49 +0300

    add

--- vue-shop/js/script.js
+++ vue-shop/js/script.js
@@ -4,11 +4,16 @@ Vue.component('product-list', {
     <div>
       <h2>{{product.name}}</h2>
       <p>{{product.desc}}</p>
-      <button>Add To Cart</button>
+      <button @click="addToCartClicked">Add To Cart</button>
       <small>{{product.id}}</small>
       <hr />
     </div>
-    `
+    `,
+    methods: {
+      addToCartClicked(){
+        this.$emit("addToCart", this.product.id);
+      }
+    }
 });
 
 Vue.component('shopping-list', {
@@ -21,10 +26,15 @@ var cartPage = new Vue({
   data: {
     shoppingList: [],
     productsList: [
-      {id: "product1", name: "Name of Product 1", desc: "Description of Product 1"},
-      {id: "product2", name: "Name of Product 2", desc: "Description of Product 2"},
-      {id: "product3", name: "Name of Product 3", desc: "Description of Product 3"},
-      {id: "product4", name: "Name of Product 4", desc: "Description of Product 4"}
-    ]
+      {id: "product1", name: "Name of Product 1", desc: "Description of Product 1", price: 10.99},
+      {id: "product2", name: "Name of Product 2", desc: "Description of Product 2", price: 20.16},
+      {id: "product3", name: "Name of Product 3", desc: "Description of Product 3", price: 50.24},
+      {id: "product4", name: "Name of Product 4", desc: "Description of Product 4", price: 0.95}
+    ],
+    methods: {
+      addToCartM: function(id) {
+        console.log(id);
+      }
+    }
   }
 });

47b5ee0b70982f95c3da3f5e07bf33d9882ce44a
Alexander Lysenko <alexlysenko@geeksforless.net>
2019-08-15 23:26:50 +0300

    add

--- vue-shop/js/script.js
+++ vue-shop/js/script.js
@@ -17,9 +17,35 @@ Vue.component('product-list', {
 });
 
 Vue.component('shopping-list', {
-  template: {}
-});
+  props: ['item'],
+  template: `
+    <div>
+      <p>{{name}}</p>
+      <p>{{item.quantity}}</p>
+      <p>{{price}}</p>
+      <hr />
+    </div>
+    `,
+    computed: {
+      name: function() { return productListColection[productsListMap[this.item.id]].name },
+      price: function() {return '$' + (productListColection[productsListMap[this.item.id]].price * this.item.quantity).toFixed(2)},
+    },
+    methods: {
 
+    }
+});
+var productListColection = [
+  {id: "product1", name: "Name of Product 1", desc: "Description of Product 1", price: 10.99},
+  {id: "product2", name: "Name of Product 2", desc: "Description of Product 2", price: 20.16},
+  {id: "product3", name: "Name of Product 3", desc: "Description of Product 3", price: 50.24},
+  {id: "product4", name: "Name of Product 4", desc: "Description of Product 4", price: 0.95}
+];
+
+var productsListMap = {};
+
+productListColection.forEach(function(pr, index) {
+  productsListMap[pr.id] = index;
+})
 
 var cartPage = new Vue({
   el: '#cart-page',
@@ -29,29 +55,34 @@ var cartPage = new Vue({
       {"id":"product3","quantity":1},
       {"id":"product2","quantity":5}
     ],
-    productsList: [
-      {id: "product1", name: "Name of Product 1", desc: "Description of Product 1", price: 10.99},
-      {id: "product2", name: "Name of Product 2", desc: "Description of Product 2", price: 20.16},
-      {id: "product3", name: "Name of Product 3", desc: "Description of Product 3", price: 50.24},
-      {id: "product4", name: "Name of Product 4", desc: "Description of Product 4", price: 0.95}
-    ]
+    productsList: productListColection
   },
-  created() {
-    //this.shoppingList = JSON.parse(localStorage.getItem('shopingCart')) || [];
+  created: function() {
+    this.changeShoppingCart();
+  },
+  watch: {
+    shoppingList: function() {
+      this.changeShoppingCart();
+    }
   },
   methods: {
     addToCartM: function(id) {
-      //this.shoppingList.push({'id': id, 'quantity': 1});
+      var newItem = true;
       for(var i=0; i<this.shoppingList.length; i++) {
-        if(this.shoppingList[i].id == id) {
-          console.log("есть такой продукт");
-          Object.assign(this.shoppingList[i], {quantity: 123})
+        var item = this.shoppingList[i];
+        if(item.id == id) {
+          Object.assign(item, {quantity: item.quantity+1});
+          newItem = false;
           return true;
         }
       }
+      if(newItem) {
+        this.shoppingList.push({'id': id, 'quantity': 1});
+      }
     },
     changeShoppingCart: function() {
-
+      // this.shoppingList = JSON.parse(localStorage.getItem('shopingCart')) || [];
+      localStorage.setItem('shopingCart', JSON.stringify(this.shoppingList))
     }
   }
 });

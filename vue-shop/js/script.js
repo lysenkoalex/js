b6290ee09084ab5cf0dfc583ebb76c07c113a823
Alexander Lysenko <alexlysenko@geeksforless.net>
2019-08-16 19:52:42 +0300

    add

--- vue-shop/js/script.js
+++ vue-shop/js/script.js
@@ -5,7 +5,7 @@ Vue.component('product-list', {
       <h2>{{product.name}}</h2>
       <p>{{product.desc}}</p>
       <button @click="addToCartClicked">Add To Cart</button>
-      <small>{{product.id}}</small>
+      <span>$ {{product.price}}</span>
       <hr />
     </div>
     `,
@@ -21,19 +21,54 @@ Vue.component('shopping-list', {
   template: `
     <div>
       <p>{{name}}</p>
-      <p>{{item.quantity}}</p>
-      <p>{{price}}</p>
+      <p>$<span>{{price}} x {{item.quantity}}</span></p>
+      <b>$<span>{{priceTotal}}</span></b>
+      <button @click="$emit('add-to-cart', id)">+</button>
+      <button @click="$emit('remove-to-cart', id)">-</button>
       <hr />
     </div>
     `,
     computed: {
+      id: function() { return productListColection[productsListMap[this.item.id]].id },
       name: function() { return productListColection[productsListMap[this.item.id]].name },
-      price: function() {return '$' + (productListColection[productsListMap[this.item.id]].price * this.item.quantity).toFixed(2)},
-    },
-    methods: {
-
+      price: function() { return productListColection[productsListMap[this.item.id]].price},
+      priceTotal: function() {return (productListColection[productsListMap[this.item.id]].price * this.item.quantity).toFixed(2)},
     }
 });
+
+Vue.component('cart-counter',{
+  props: ['propsList'],
+  template: `
+    <span>{{count}}</span>
+  `,
+  computed: {
+    count: function(){
+      var totalCount = 0;
+      for(var i=0; i < this.propsList.length; i++){
+        totalCount += this.propsList[i].quantity;
+      }
+      return totalCount;
+    }
+  }
+});
+
+Vue.component('total-line',{
+  props: ['propsList'],
+  template: `
+    <span v-if="propsList.length"><b>Total:</b> {{total}}</span>
+    <h4 class="text-center" v-else>Your cart is empty</h4>
+  `,
+  computed: {
+    total: function(){
+      var totalPrice = 0;
+      for(var i=0; i < this.propsList.length; i++){
+        totalPrice += productListColection[productsListMap[this.propsList[i].id]].price *  this.propsList[i].quantity;
+      }
+      return totalPrice.toFixed(2);
+    }
+  }
+});
+
 var productListColection = [
   {id: "product1", name: "Name of Product 1", desc: "Description of Product 1", price: 10.99},
   {id: "product2", name: "Name of Product 2", desc: "Description of Product 2", price: 20.16},
@@ -50,19 +85,18 @@ productListColection.forEach(function(pr, index) {
 var cartPage = new Vue({
   el: '#cart-page',
   data: {
-    shoppingList: [
-      {"id":"product1","quantity":10},
-      {"id":"product3","quantity":1},
-      {"id":"product2","quantity":5}
-    ],
+    shoppingList: [],
     productsList: productListColection
   },
   created: function() {
-    this.changeShoppingCart();
+    this.shoppingList = JSON.parse(localStorage.getItem('shopingCart')) || [];
   },
   watch: {
-    shoppingList: function() {
-      this.changeShoppingCart();
+    shoppingList: {
+      deep: true,
+      handler: function() {
+        localStorage.setItem('shopingCart', JSON.stringify(this.shoppingList));
+      }
     }
   },
   methods: {
@@ -80,9 +114,20 @@ var cartPage = new Vue({
         this.shoppingList.push({'id': id, 'quantity': 1});
       }
     },
+    removeToCartM: function(id) {
+      for(var i=0; i<this.shoppingList.length; i++) {
+        var item = this.shoppingList[i];
+        if(item.id == id ) {
+          if ( item.quantity > 1 ){
+            Object.assign(item, {quantity: item.quantity-1});
+          } else {
+            this.shoppingList.splice(i, 1);
+            return;
+          }
+        }
+      }
+    },
     changeShoppingCart: function() {
-      // this.shoppingList = JSON.parse(localStorage.getItem('shopingCart')) || [];
-      localStorage.setItem('shopingCart', JSON.stringify(this.shoppingList))
     }
   }
 });
